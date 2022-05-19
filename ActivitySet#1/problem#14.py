ebc5453243c05c25d8d8d5e0afb46ae929d8c476
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-05-19 22:56:59 +0000

    done

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,19 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+import re
+hand=open("actual.txt")
+count=0
+total=0
+for line in hand:
+    line=line.strip("") 
+    num=re.findall('[0-9]+',line)
+    try:
+        for i in num:
+            if int(i):
+                print(i)
+                total=total+int(i)
+                count=count+1
+    except:
+        continue
+print("TOTAL COUNT:- ",count)
+print("SUM:- ",total)
