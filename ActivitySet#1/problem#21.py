7f92e4b8dd078b93256ed4c4301462e1a90c1918
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-05-30 23:04:06 +0000

    Done

--- ActivitySet#1/problem#21.py
+++ ActivitySet#1/problem#21.py
@@ -1,1 +1,25 @@
-import mysql.connector as ms
+'''
+create database ages;
+
+use ages;
+
+CREATE TABLE Ages 
+( 
+name VARCHAR(128), 
+age INTEGER
+);
+
+DELETE FROM Ages;
+INSERT INTO Ages (name, age) VALUES ('Jaymie', 17);
+INSERT INTO Ages (name, age) VALUES ('Chizaram', 35);
+INSERT INTO Ages (name, age) VALUES ('Macaulay', 13);
+INSERT INTO Ages (name, age) VALUES ('Brodi', 17);
+INSERT INTO Ages (name, age) VALUES ('Milie', 30);
+INSERT INTO Ages (name, age) VALUES ('Fearn', 21);
+'''
+#for sql_lite
+#SELECT hex(name||age) AS X FROM Ages ORDER BY X;
+#for MySQL
+#select hex(concat(name,age)) as x from ages order by x;
+
+#ANSWER:- 42726F64693137
