e944dc7770816a1253f1d2dbdf4c0c88e63f0cbb
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-06-02 23:18:44 +0000

    done

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,12 +1,12 @@
 
 
-def add(a, b):
-    return  # ...
+def add(p,q):
+    return p+q
 
 
 def main():
-    a = input  # ...
-    b = input  # ...
-
-    c = add(a, b)
-    print  # ...
+    a = input("Enter 1st number of choice:-")
+    b = input("Enter 2nd number of choice:- ")
+    c = add(int(a),int(b))
+    print(c)
+main()
