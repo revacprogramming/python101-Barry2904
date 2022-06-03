c00f863c74842ccbad1fba2dc341ebecaea7baeb
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-06-03 23:19:38 +0000

    Done

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,17 +1,13 @@
+def add(a,b):
+  return int(a)+int(b)
 
-def add(a, b):
-    pass  # ...
-
-
-def output(a, b, sum):
-    pass  # ...
-
+def output(a,b,sum):
+  print(a,"+",b,"is",sum)
 
 def main():
-    a, b = input_two_numbers()
-    sum = add(a, b)
-
-    output(a, b, sum)
+    a,b=input("Enter two numbers:- ").split()
+    sum=add(a,b)
+    output(a,b,sum)
 
 
 if __name__ == '__main__':
