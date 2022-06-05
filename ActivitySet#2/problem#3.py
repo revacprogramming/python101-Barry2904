07769ddac4d139ad45e9589b1ae2d774ca3d8e31
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-06-05 04:49:44 +0000

    Done

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,19 +1,22 @@
-
-
 def get_cs():
-    """get string input"""
-
+  """get string input"""
+  k=input("Enter the credentials:- ")
+  return k
 
 def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
-
+  """convert connected string to list of strings"""
+  l=list()
+  k=cs.split(";")
+  for i in k:
+    p=i.split("=")
+    n=tuple(p)
+    l.append(n)
+  return l
+  
 def main():
-    cs = get_cs()
-
-    lot = cs_to_lot(cs)
-    print(lot)
-
-
+  cs = get_cs()
+  lot = cs_to_lot(cs)
+  print(lot)
+  
 if __name__ == '__main__':
-    main()
+  main()
