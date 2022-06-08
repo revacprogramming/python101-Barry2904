82843ec76896d27582acd1f4cd93065addae77b5
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-06-08 04:00:00 +0000

    Done

--- ActivitySet#1/problem#22.py
+++ ActivitySet#1/problem#22.py
@@ -1,1 +1,13 @@
-#i was testing 
+class fibo:
+  def __init__(self):
+    self.a=0
+    self.b=1
+  def __next__(self):
+    yield self.a
+    self.a=self.b
+    self.b=self.b+self.a
+    return self.a
+x=fibo()
+print(x)
+# for i in x:
+#   yield i
