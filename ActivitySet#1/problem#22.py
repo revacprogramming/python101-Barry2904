8a6bf3de0accb4b8c52d60fad719ddec185cd0b7
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-06-09 15:14:03 +0000

    done

--- ActivitySet#1/problem#22.py
+++ ActivitySet#1/problem#22.py
@@ -1,13 +1,14 @@
-class fibo:
-  def __init__(self):
-    self.a=0
-    self.b=1
-  def __next__(self):
-    yield self.a
-    self.a=self.b
-    self.b=self.b+self.a
-    return self.a
-x=fibo()
-print(x)
-# for i in x:
-#   yield i
+# class fibo:
+#   def __init__(self):
+#     self.a=0
+#     self.b=1
+#   def __next__(self):
+#     yield self.a
+#     self.a=self.b
+#     self.b=self.b+self.a
+#     return self.a
+# x=fibo()
+# print(x)
+# # for i in x:
+# #   yield i
+print("The mangy, scrawny stray dog %s gobbled down" + "the grain-free, organic dog food." %'hurriedly')
