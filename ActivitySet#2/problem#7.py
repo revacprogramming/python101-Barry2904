68d041bbae0561cda8cab360931731136f6d8e2a
Barry2904 <bharathjramalingam2467@gmail.com>
2022-07-04 05:10:16 +0530

    done

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,8 +1,19 @@
-
 class Menu:
-    """fill in class definition here"""
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def __add__(self,other):
+        return self.x + other.x ,self.y + other.y
+        '''
+        x = self.x + other.x
+        y = self.y + other.y
+        return Menu(x,y)'''
 
-m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
-
-print(m)  # should print the menu properly
+# m = Menu()
+a= Menu("idly ", 'vada ')
+b= Menu("10", '20')
+# m = m + Menu("idly", '10') + Menu("vada", '20')  
+# Hint: operator overloading special methods (__add__, __sub__, etc.)
+c=a+b
+for i in c:
+    print(i)
