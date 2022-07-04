913a4b706715f7db3c33b0814026d60441fae593
Barry2904 <bharathjramalingam2467@gmail.com>
2022-07-04 14:42:41 +0530

    done

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,19 +1,18 @@
+# Hint: operator overloading special methods (__add__, __sub__, etc.)
 class Menu:
-    def __init__(self,x,y):
+    def __init__(self,x='',y=''):
         self.x=x
         self.y=y
     def __add__(self,other):
         return self.x + other.x ,self.y + other.y
-        '''
-        x = self.x + other.x
-        y = self.y + other.y
-        return Menu(x,y)'''
 
-# m = Menu()
+#==============METHOD 1==========
 a= Menu("idly ", 'vada ')
 b= Menu("10", '20')
-# m = m + Menu("idly", '10') + Menu("vada", '20')  
-# Hint: operator overloading special methods (__add__, __sub__, etc.)
 c=a+b
 for i in c:
     print(i)
+#==============METHOD 2==========
+m = Menu("idly ", 'vada ') + Menu("10", '20')
+print(m)  
+
