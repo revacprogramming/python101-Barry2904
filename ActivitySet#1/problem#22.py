99e61c77121599ef65143292eeda2d5a881d8a91
Bharath J Ramalingam <96277696+Barry2904@users.noreply.github.com>
2022-07-24 03:04:27 +0000

    done

--- ActivitySet#1/problem#22.py
+++ ActivitySet#1/problem#22.py
@@ -1,18 +1,43 @@
-# class fibo:
-#   def __init__(self):
-#     self.a=0
-#     self.b=1
-#   def __next__(self):
-#     yield self.a
-#     self.a=self.b
-#     self.b=self.b+self.a
-#     return self.a
-# x=fibo()
-# print(x)
-# # for i in x:
-# #   yield i
-print("The mangy, scrawny stray dog %s gobbled down" + "the grain-free, organic dog food." %'hurriedly')
+import sqlite3
 
+conn = sqlite3.connect('emaildb2.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+DROP TABLE IF EXISTS Counts''')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+list_1 =[]
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    dom = email.find('@')
+    org = email[dom+1:len(email)]
+
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+conn.commit()
+''' https://www.sqlite.org/lang_select.html'''
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
 
 
 
